b12ce165197dc2a628152b0eae14572f8c27f266
jiveeee <m.javad.raisi@gmail.com>
2022-07-19 16:46:32 +0200

    refactors the listItemElement creation

--- main.js
+++ main.js
@@ -15,10 +15,24 @@ newTodoFormElement.onsubmit = (event) => {
 function createNewTodoItem(value) {
     if (value === "") return
     const listItemElement = document.createElement('li')
+    const labelElement = document.createElement('label')
+    const inputElement = document.createElement('input')
+    inputElement.type = 'checkbox'
+    const textNode = document.createTextNode(value)
+    console.log(textNode)
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = 'Delete'
+
+    console.log(value)
+    console.log(value)
+    labelElement.appendChild(inputElement)
+    labelElement.appendChild(textNode)
+    listItemElement.appendChild(labelElement)
+    listItemElement.appendChild(buttonElement)
     
-    listItemElement.innerHTML = `
-        <label><input type="checkbox">${value}</label>
-        <button>Delete</button>
-    `
+    // listItemElement.innerHTML = `
+    //     <label><input type="checkbox">${value}</label>
+    //     <button>Delete</button>
+    // `
     return listItemElement
 }
